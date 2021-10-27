cf05565580da6a9308fcce8fa912514e854f17c0
Johann Vivas <yovivstolkan@gmail.com>
2021-10-28 00:08:42 +0800

    added logout function, added small bit of error text for login

---
+++ .history/account/views_20211027203653.py
@@ -0,0 +1,67 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from account.forms import RegistrationForm, LGURegistrationForm, LoginForm
+
+# Create your views here.
+
+def login_view(request):
+    context = {}
+    if request.POST:
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data.get("email")
+            print(email)
+            raw_password = form.cleaned_data.get("password")
+            print(raw_password)
+            account = authenticate(email = email, password = raw_password)
+            print(account)
+            if account != None:
+                context['user'] = account
+                login(request, account)
+                return redirect("communityboard", context)
+            else:
+                context['login_form'] = form
+        else:
+            context['login_form'] = form
+    else:
+        form = LoginForm()
+        context['login_form'] = form
+    return render(request, "chart/login.html", context)
+
+def docregistration_view(request):
+    context = {}
+    if request.POST:
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            email = form.cleaned_data.get("email")
+            raw_password = form.cleaned_data.get("password1")
+            account = authenticate(email=email, password=raw_password)
+            login(request, account)
+            return redirect("communityboard")
+        else: #not a valid form
+            context['registration_form'] = form
+    else: #not POST request, it's a GET request; this is their first time seeing this
+        form = RegistrationForm()
+        context['registration_form'] = form
+    return render(request, "chart/docregister.html", context)
+
+def lguregistration_view(request):
+    context={}
+    if request.POST:
+        form = LGURegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            email = form.cleaned_data.get("email")
+            raw_password = form.cleaned_data.get("password1")
+            account = authenticate(email=email, password=raw_password)
+            login(request, account)
+            return redirect("communityboard")
+        else: #not a valid form
+            context['registration_form'] = form
+    else: #not POST request, it's a GET request; this is their first time seeing this
+        form = LGURegistrationForm()
+        context['registration_form'] = form
+    return render(request, "chart/lguregister.html", context)
+            
+
