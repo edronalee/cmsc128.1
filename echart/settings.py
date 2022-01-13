8c2795cb1ccec26055dc2ffca48b2b33bb95618a
Lady Edronalee <ljtavu@up.edu.ph>
2022-01-13 14:36:22 +0800

    modify settings

--- echart/settings.py
+++ echart/settings.py
@@ -133,8 +133,6 @@ USE_TZ = False
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-
 STATICFILES_DIRS = ( 
     os.path.join(BASE_DIR, 'static'), 
 )
